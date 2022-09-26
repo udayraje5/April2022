07ef0820580924dafdf316fca7af11955fbb75fe
udayraje5 <uday.arsolikar@gmail.com>
2022-09-27 00:48:35 +0530

    This is my second commit

--- src/test/java/com/Git/Pages/TestClass.java
+++ src/test/java/com/Git/Pages/TestClass.java
@@ -1,5 +1,22 @@
 package com.Git.Pages;
 
-public class TestClass {
+import org.testng.annotations.Test;
+
+public class TestClass
+{
+	@Test
+	public void testCase1() 
+	{
+		System.out.println("Hello Good Morning GitHub");
+		
+	}
+	@Test
+	public void testCase2() 
+	{
+		System.out.println("This is my first commit");
+		
+	}
+
+	
 
 }
